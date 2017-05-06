8707bc4a8647b507459ced117ea3e75af2334532
spycebot <photius@shannonware.com>
2017-05-06 13:52:08 +0100

    Add multiple cameras

--- js/shannonware.js
+++ js/shannonware.js
@@ -7,12 +7,13 @@ var SCREEN_HEIGHT = window.innerHeight;
 var aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
 
 var container; // Mr.doob road minus stats lib
-var camera, scene, renderer, mesh;
-var cameraRig, activeCamera, activeHelper;
+var camera, scene, renderer, raycaster, mesh;
+var cameraRig, activeCamera, activeHelper, showHelpers;
 var initialCameraHelper, orbitCameraHelper;
-var orbitCamera, tetrahedron, plane;
+var orbitCamera, orthographicCamera, tetrahedron, plane;
 //var frustumSize = 600;
 var clock = new THREE.Clock();
+var mouse = new THREE.Vector2(), INTERSECTED; // https://github.com/mrdoob/three.js/blob/master/examples/webgl_interactive_cubes.html
 
 
 function init(){
@@ -22,21 +23,27 @@ function init(){
 	//scene.fog = new THREE.Fog( 0x808080, 2000, 4000 );
 	//var geometry = new THREE.Geometry();
 	/*
+		https://github.com/mrdoob/three.js/blob/master/examples/webgl_interactive_cubes.html
+	*/
+	raycaster = new THREE.Raycaster();
+	/*
 		Set up renderer
 	*/
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer( { antialias: true , alpha: true} );
+	//renderer = new THREE.CanvasRenderer( { alpha: true }); // gradient
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
 	renderer.domElement.style.position = "relative";
 	container.appendChild( renderer.domElement );
 	//renderer.autoClear = false; // ????
-	renderer.setClearColor( 0xffffff, 1.0 );
+	renderer.setClearColor( 0xffffff, 0 );
 
 
 	/*
 		Respond to device changes
 	*/
 	window.addEventListener( 'resize', onWindowResize, false );
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 	/*
 	document.addEventListener( 'keydown', onKeyDown, false );
 	*/
@@ -47,18 +54,26 @@ function init(){
 function setupCameras() {
 	camera = new THREE.PerspectiveCamera( 50, 0.5 * aspect, 1, 10000 );
 	orbitCamera = new THREE.PerspectiveCamera( 75, 0.5 * aspect, 1, 10000 );
+	orthographicCamera = new THREE.OrthographicCamera( SCREEN_WIDTH / - 20, SCREEN_WIDTH / 20, SCREEN_HEIGHT / 20, SCREEN_HEIGHT / - 20, 1, 10000 );
 	initialCameraHelper = new THREE.CameraHelper( camera );
 	scene.add( initialCameraHelper );
 	orbitCameraHelper = new THREE.CameraHelper( orbitCamera );
 	scene.add( orbitCameraHelper );
+	orthographicCameraHelper = new THREE.CameraHelper( orthographicCamera );
+	scene.add( orthographicCamera );
 	//camera = new THREE.OrthographicCamera( 0.5 * frustumSize * aspect / - 2, 0.5 * frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 150, 1000 );
 	camera.position.z = 50;
 	camera.position.y = 10;
 	orbitCamera.position.z = 50;
 	orbitCamera.position.y = 10;
 	// cameraInitialHelper, cameraOrbitHelper;
+	orthographicCamera.position.y = 40;
+	orthographicCamera.position.z = 40;
+	orthographicCamera.position.x = -40;
+	//orthographicCamera.rotation.z = (Math.PI / 2 , 0, 0);
 
 	activeCamera = camera;
+	showHelpers = true;
 	//activeHelper = cameraPerspectiveHelper;
 	//camera.lookAt(tetrahedron.position);
 	console.log("setupCameras complete.");
@@ -104,17 +119,17 @@ function setupScene() {
 	cube.position.x = 20;
 	cube.position.y = 5;
 
-	var octahedronGeometry = new THREE.OctahedronGeometry(10,0);
+	var octahedronGeometry = new THREE.OctahedronGeometry(5,0);
 	var octahedron = new THREE.Mesh( octahedronGeometry, material) ;
 	octahedron.position.x = 40;
 	octahedron.position.y = 5;
 	scene.add( octahedron );
-	var dodecahedronGeometry = new THREE.DodecahedronGeometry(10,0);
+	var dodecahedronGeometry = new THREE.DodecahedronGeometry(5,0);
 	var dodecahedron = new THREE.Mesh( dodecahedronGeometry, material) ;
 	dodecahedron.position.x = 60;
 	dodecahedron.position.y = 5;
 	scene.add( dodecahedron );
-	var icosahedronGeometry = new THREE.IcosahedronGeometry(10,0);
+	var icosahedronGeometry = new THREE.IcosahedronGeometry(5,0);
 	var icosahedron = new THREE.Mesh( icosahedronGeometry, material) ;
 	icosahedron.position.x = 80;
 	icosahedron.position.y = 5;
@@ -122,6 +137,14 @@ function setupScene() {
 
 	console.log("setupScene complete.");
 }
+
+function onDocumentMouseMove( event ) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	// console.log("Mouse x,y: " + mouse.x + ", " + mouse.y);
+}
+
 
 function animateObjects() {
 	tetrahedron.rotation.y += 0.01
@@ -131,14 +154,19 @@ function animateObjects() {
 	orbitCamera.position.x = fudgeDistance * Math.cos( r );
 	orbitCamera.position.z = fudgeDistance * Math.sin( r );
 	orbitCamera.lookAt(tetrahedron.position);
+	orthographicCamera.lookAt(tetrahedron.position);
 }
 
 function onWindowResize(event) {
 	SCREEN_WIDTH = window.innerWidth;
 	SCREEN_HEIGHT = window.innerHeight;
 	aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+
+	$(".guiButton").css({"font-size" : (SCREEN_HEIGHT+SCREEN_WIDTH)/40 });
+
 	renderer.setPixelRatio( window.devicePixelRatio );
-	console.log("Device pixel ratio: " + window.devicePixelRatio);
+	//console.log("Device pixel ratio: " + window.devicePixelRatio);
 	renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
 
 	camera.aspect = aspect ; //0.5 * aspect;
@@ -157,6 +185,24 @@ function render(){
 	requestAnimationFrame( render );
 	animateObjects();
 	executeJQuery();
+	/*
+		https://github.com/mrdoob/three.js/blob/master/examples/webgl_interactive_cubes.html
+	*/
+	raycaster.setFromCamera( mouse, camera );
+	/*
+	var intersects = raycaster.intersectObjects( scene.children );
+	if ( intersects.length > 0 ) {
+		console.log("Intersects.length = " + intersects.length + "; INTERSECTED: " + typeof intersects);
+		if ( INTERSECTED != intersects[ 0 ].object ) {
+			if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+			INTERSECTED = intersects[ 0 ].object;
+			INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+			INTERSECTED.material.emissive.setHex( 0xff0000 );
+		}
+	} else {
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+		INTERSECTED = null;
+	}
 	/*
 		Conform to this
 	*/
@@ -192,10 +238,51 @@ function executeJQuery() {
 }
 
 $(function() {
+	$(".guiButton").css({"font-size" : (SCREEN_HEIGHT+SCREEN_WIDTH)/40 }); /* two of two cf. on window resize */
 	$("#cameraButton").click(function() {
-		if (activeCamera == orbitCamera ) { activeCamera = camera ; } else { activeCamera = orbitCamera ;}
+		//if (activeCamera == orbitCamera ) { activeCamera = camera ; } else { activeCamera = orbitCamera ;}
+		console.log("activeCamera: " + activeCamera.isCamera);
+		switch (activeCamera) {
+			case camera:
+				activeCamera = orbitCamera;
+			break;
+			case orbitCamera:
+				activeCamera = orthographicCamera;
+				console.log ("Ortho camera position: " + orthographicCamera.position.x.toString() + ", " + orthographicCamera.position.y.toString() + ", " + orthographicCamera.position.z.toString() + "; rotation: " + orthographicCamera.rotation.x.toString() + ", " + orthographicCamera.rotation.y.toString() + ", " + orthographicCamera.rotation.z.toString());
+			break;
+			case orthographicCamera:
+				activeCamera = camera;
+			break;
+			default: 
+				console.log("Correct camera not chosen.")
+			break;
+		}
 		console.log("Switch button pressed.");
 	})
+	$("#helpersButton").click(function() {
+		if (showHelpers) { 
+			showHelpers = false; 
+			scene.remove(initialCameraHelper);
+			scene.remove(orbitCameraHelper);
+			scene.remove(orthographicCameraHelper);
+		} else { 
+			showHelpers = true ;
+			scene.add(initialCameraHelper);
+			scene.add(orbitCameraHelper);
+			scene.add(orthographicCameraHelper);
+		}
+		console.log("showHelpers set to " + showHelpers.toString());
+	})
+	$("#guiToggleButton").click(function() { 
+		/*
+		$("#guiToggleButton").toggle(function(){
+			$(this).text("Open Controls");
+		}, function() {
+			$(this).text("Close Controls");
+		});
+		*/
+		$('#myGUI').children("#myGUI > h1, #myTime, #cameraButton, #helpersButton").slideToggle('slow'); 
+	});
 	console.log("End of jQuery");
 
 });
